9f2dd201a1c5513b580acab9eb052af92e109054
Sashwath <ksashwathkishore@gmail.com>
2026-09-18 03:58:55 -0700

    Containerize PipelineIQ backend and dashboard

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,17 +8,56 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 
-DATABASE_URL = (
-    f"postgresql+psycopg://"
-    f"{os.getenv('POSTGRES_USER')}:"
-    f"{os.getenv('POSTGRES_PASSWORD')}@"
-    f"localhost:"
-    f"{os.getenv('POSTGRES_PORT', '5432')}/"
-    f"{os.getenv('POSTGRES_DB')}"
+POSTGRES_HOST = os.getenv(
+    "POSTGRES_HOST",
+    "localhost",
+)
+
+POSTGRES_PORT = os.getenv(
+    "POSTGRES_PORT",
+    "5432",
+)
+
+POSTGRES_DB = os.getenv(
+    "POSTGRES_DB",
+)
+
+POSTGRES_USER = os.getenv(
+    "POSTGRES_USER",
+)
+
+POSTGRES_PASSWORD = os.getenv(
+    "POSTGRES_PASSWORD",
 )
 
 
-engine = create_engine(DATABASE_URL)
+if not all(
+    [
+        POSTGRES_DB,
+        POSTGRES_USER,
+        POSTGRES_PASSWORD,
+    ]
+):
+    raise RuntimeError(
+        "PostgreSQL environment variables are not configured."
+    )
+
+
+DATABASE_URL = (
+    f"postgresql+psycopg://"
+    f"{POSTGRES_USER}:"
+    f"{POSTGRES_PASSWORD}@"
+    f"{POSTGRES_HOST}:"
+    f"{POSTGRES_PORT}/"
+    f"{POSTGRES_DB}"
+)
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
 
 SessionLocal = sessionmaker(
     bind=engine,
